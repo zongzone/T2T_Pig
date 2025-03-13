9c51c1c29ac1e294c6d8d92827f05baafafa4a65
zong <101814626+zongzone@users.noreply.github.com>
2025-03-13 14:39:13 +0800

    Update GS.R

--- 8.Breeding analysis/GS.R
+++ 8.Breeding analysis/GS.R
@@ -3,45 +3,47 @@ library(rrBLUP)
 library(caret)
 library(readr)
 
-x <- as.matrix(read.table("~/sv_impute.txt", header = F))  
-y <- as.matrix(read.table("~/phe.txt", header = T))
+x <- as.matrix(read.table("~sv.txt", header = T))
+y <- as.matrix(read.table("~phe.txt", header = T)) 
 colnames(x) <- as.character(seq_len(ncol(x)))
-
-
-dtrain_full <- lgb.Dataset(data = as.matrix(x), label = y)
-params <- list(objective = "regression", metric = "mse")
-lgb_model_full <- lgb.train(params, dtrain_full)
-
-
-importance_full <- lgb.importance(lgb_model_full, percentage = TRUE)
-important_features <- importance_full[importance_full$Gain > 0, "Feature"]
-important_features <- as.character(important_features$Feature)
-filtered_x <- as.matrix( x[, important_features])
-
 
 set.seed(123)
 n_repeats <- 10
-cor_results <- list() 
-
+cor_results <- list()
 
 for (rep in 1:n_repeats) {
-  # 生成新的交叉验证折
-  folds <- createFolds(y, k = 10, list = TRUE, returnTrain = FALSE)
-  cor_list <- c()  # 存储当前重复的相关系数 
+  
+  folds <- createFolds(y, k = 10, list = TRUE)
+  cor_list <- c()  
+  
   for (i in 1:10) {
-	test_index <- folds[[i]]
-	train_index <- setdiff(1:nrow(filtered_x), test_index)
-	train_x <- as.matrix(filtered_x[train_index,])
-	test_x <- as.matrix(filtered_x[test_index,])
-	train_y <- y[train_index]
-	test_y <- y[test_index]
-		
-	rrblup_model <- mixed.solve(train_y, Z = train_x)
-	predicted_y <- test_x %*% rrblup_model$u
-
-	cor_val <- cor(predicted_y, test_y)
-	cor_list <- c(cor_list, cor_val)
+    test_index <- folds[[i]]
+    train_index <- setdiff(1:nrow(x), test_index)
+    
+    train_x <- as.matrix(x[train_index, , drop = FALSE])
+    test_x <- as.matrix(x[test_index, , drop = FALSE])
+    train_y <- as.matrix(y[train_index])
+    test_y <- as.matrix(y[test_index])
+   	
+    dtrain <- lgb.Dataset(data = train_x, label = train_y)
+    params <- list(objective = "regression", metric = "mse")
+    lgb_model <- lgb.train(params, dtrain)
+        
+    importance <- lgb.importance(lgb_model, percentage = TRUE)
+    important_features <- as.character(importance$Feature[importance$Gain > 0])
+        
+    train_x_filtered <- as.matrix(train_x[, important_features, drop = FALSE])
+    test_x_filtered <- as.matrix(test_x[, important_features, drop = FALSE])
+    
+    #mode(train_x_filtered) <- "numeric"
+    #mode(test_x_filtered) <- "numeric"
+    rrblup_model <- mixed.solve(train_y, Z = train_x_filtered)
+    predicted_y <- test_x_filtered %*% rrblup_model$u
+    
+    cor_val <- cor(predicted_y, test_y)
+    cor_list <- c(cor_list, cor_val)
   }
+  
   cor_results[[rep]] <- cor_list
 }
 
