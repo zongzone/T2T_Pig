74376db07e3ef489e9b179c64e8110607e1fa1a3
zong <101814626+zongzone@users.noreply.github.com>
2026-01-18 11:48:47 +0800

    Update genome assembly script with additional notes
    Added notes regarding Hi-C reads and YaHS recommendation for chromosome-scale scaffolding.

--- 1.Genome assembly/genome assembly.sh
+++ 1.Genome assembly/genome assembly.sh
@@ -2,10 +2,11 @@
 
 hifiasm -o minzhu -t 30 --ul ONT.reads --h1 hic.reads1 --h2 hic.reads2 HiFi.reads
 
+Note: Removing the Hi-C reads typically yields the smallest number of contigs (i.e., a less fragmented contig set), whereas including Hi-C reads often increases the contig N50 and produces longer contigs overall.
+
 nextDenovo nextdenovo.cfg
 
 nextPolish run.cfg
-
 
 #Chromosome mounting 
 
@@ -13,6 +14,8 @@ allhic extract group.clean.bam group.fasta --RE GATC
 partition --pairsfile group.clean.pairs.txt --contigfile group.clean.counts_GATC.txt -K 19 --minREs 50 --maxlinkdensity 3 --NonInformativeRabio 2
 allhic build group.fasta
 allhic optimize group1.txt group.clean.clm
+
+Note: Although the commands below are functional, we now generally recommend using **YaHS** for chromosome-scale scaffolding/anchoring, as it tends to be more robust and easier to tune across datasets.
 
 #Gap filling
 
